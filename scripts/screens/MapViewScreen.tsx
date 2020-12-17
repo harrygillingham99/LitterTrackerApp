0ca7a2782aa684c7d7a023deaf88686473fcaa77
harrygillingham99 <harrygillingham99@gmail.com>
2020-12-17 17:26:09 +0000

    test(pre-commit)

--- scripts/screens/MapViewScreen.tsx
+++ scripts/screens/MapViewScreen.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { AppHeader } from "../components/nav/Header";
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import { DrawerScreens } from "../types/nav/DrawerScreens";
+import { Routes } from "../types/nav/Routes";
+import { AppLogoIcon } from "../components/AppLogoIcon";
 
-export const MapViewScreen = () => {
+type MapViewScreenNavigationProp = DrawerNavigationProp<
+  DrawerScreens,
+  Routes.MapView
+>;
+
+type MapViewScreenProps = {
+  navigation: MapViewScreenNavigationProp;
+};
+
+export const MapViewScreen = (props: MapViewScreenProps) => {
   return (
     <View style={styles.container}>
+      <AppHeader
+        leftComponentOnPress={props.navigation.toggleDrawer}
+        centerComponent={AppLogoIcon}
+      />
       <Text>Map HEre</Text>
       <StatusBar style="auto" />
     </View>
