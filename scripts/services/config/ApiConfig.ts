74ba6b7264028e0ff6ad1487b0b4ee79e09af7d7
harrygillingham99 <harrygillingham99@gmail.com>
2021-01-11 14:10:22 +0000

    fix: comment

--- scripts/services/config/ApiConfig.ts
+++ scripts/services/config/ApiConfig.ts
@@ -5,6 +5,6 @@
 
 const uri = `http://192.168.0.69:82`
 
-const prodUri = `https://litter-tracker.ew.r.appspot.com` //TODO: swap this URL when its time to hand in & deploy latest backend
+const prodUri = `https://litter-tracker.ew.r.appspot.com` //TODO: swap to this URL when its time to hand in & deploy latest backend
 
 export const ApiUrl = prodUri;
