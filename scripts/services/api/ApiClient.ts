a009a23f3fbdaec76f280e16fcd16594b4488e82
harrygillingham99 <harrygillingham99@gmail.com>
2021-01-11 13:55:25 +0000

    feat(comment)

--- scripts/services/api/ApiClient.ts
+++ scripts/services/api/ApiClient.ts
@@ -5,13 +5,14 @@ export class IConfig {
     ApiClient.ts
     This is the base classes for the NSwag generated ApiClient. Has overrides for transformOptions and getBaseUrl to allow me to instantiate a client and
     inject firebase authorization tokens into the request header for the back end to then verify and get my own baseUrl stored in a config file.
+    A comment at the top of this file will actually break client generation and for whatever reason this will end up at the bottom of Client.ts not the top.
   */
   constructor(token: string) {
     this.JwtToken = token;
   }
-  /**
-   * Returns a valid value for the Authorization header.
-   * Used to dynamically inject the current auth header.
+  /*
+    Returns a valid value for the Authorization header.
+    Used to dynamically inject the current auth header.
    */
   JwtToken: string;
 }
